6f14795477636dd29be9c2459c6343be4fd67be4
Haider Al-Hakeem <hah288@nyu.edu>
2018-12-14 16:17:18 -0500

    added nonlog scatterplot

--- final_part_A_hah288.py
+++ final_part_A_hah288.py
@@ -5,9 +5,16 @@ import warnings # Ignores warnings from seaborn
 warnings.filterwarnings("ignore")
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 tetrahymena = pd.read_table('tetrahymena.tsv')
+
 tetrahymena = tetrahymena[tetrahymena.diameter >= 19.2]
 tetrahymena = tetrahymena[tetrahymena.diameter <= 26.0]
 tetrahymena_means = tetrahymena.groupby(['culture','glucose'],as_index=False).agg({'conc':'mean','diameter':'mean'})
+
+plot = sns.FacetGrid(tetrahymena_means, hue="glucose", size=5, hue_order=["glucose_yes", "glucose_no"], hue_kws=dict(marker=["o", "s"])) \
+   .map(plt.scatter, "diameter", "conc") \
+   .add_legend()
+plt.title("Nonlog Concentration versus Diameter")
+plt.show()
+plot.savefig('final_part_A_nonlog_hah288.pdf')
 
